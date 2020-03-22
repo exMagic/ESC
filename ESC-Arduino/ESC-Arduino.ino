b9200e24f75f742500cce3b5112c04a6dd43d954
Maciej Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2020-03-22 17:54:01 +0100

    it's alive

--- ESC-Arduino/ESC-Arduino.ino
+++ ESC-Arduino/ESC-Arduino.ino
@@ -1,141 +1,69 @@
 
+int AH = 13;
+int AL = 12;
+int BH = 11;
+int BL = 10;
+int CH = 9;
+int CL = 8;
 
-
-int AA1 = 13;
-int AA2 = 12;
-int BB1 = 11;
-int BB2 = 10;
-int CC1 = 9;
-int CC2 = 8;
-
-int enable = 2;
-int emfA = A0;
-int emfB = A1;
-int emfC = A2;
-
-int fase = 1;
-
-int IN = A3;
-int tiempo = 50;
-
-int mx = 50;
-
-
-unsigned long previousMillis = millis();
+int f_max = 100;
+int f_delay = 3 ;
+int loopSpeed = 1;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(AA1, OUTPUT);
-  pinMode(AA2, OUTPUT);
-  pinMode(BB1, OUTPUT);
-  pinMode(BB2, OUTPUT);
-  pinMode(CC1, OUTPUT);
-  pinMode(CC2, OUTPUT);
+  pinMode(AH, OUTPUT);//RED - OSC_ORANGE
+  pinMode(AL, OUTPUT);//RED
+  pinMode(BH, OUTPUT);//GREEN - OSC_BROWN
+  pinMode(BL, OUTPUT);//GREEN
+  pinMode(CH, OUTPUT);//BLUE - OSC_WHITE
+  pinMode(CL, OUTPUT);//BLUE
 
-  pinMode(enable, OUTPUT);
+  analogWrite(AH, 0);
+  analogWrite(AL, 0);
+  analogWrite(BH, 0);
+  analogWrite(BL, 0);
+  analogWrite(CH, 0);
+  analogWrite(CL, 0);
+}
+void phase(int off_1, int off_2, int off_3, int on, int up, int down) {
 
-  pinMode(IN, INPUT);
-  pinMode(emfA, INPUT);
-  pinMode(emfB, INPUT);
-  pinMode(emfC, INPUT);
+  digitalWrite(off_1, LOW);// !IMPORTANT
+  digitalWrite(off_2, LOW);// !IMPORTANT
+  digitalWrite(off_3, LOW);// !IMPORTANT
 
+  digitalWrite(on, HIGH); //ON
+  analogWrite(up, f_max); //GO UP
+  digitalWrite(down, 0);//OFF
+  delay(f_delay);
 
-  digitalWrite(enable, HIGH);
-  previousMillis = micros();
-
-
+  //  for (int i = 0; i <= f_max; i = i + loopSpeed) {
+  //
+  //    analogWrite(up, i); //GO UP
+  //    analogWrite(down, f_max - i);//GO DOWN
+  //
+  //    delay(f_delay);
+  //
+  //  }
 
 
 }
-
 void loop() {
 
-  unsigned long currentMillis = millis();
-
-
-  //Serial.println(currentMillis - previousMillis);
-  //Serial.println(tiempo);
-  
-  if (currentMillis - previousMillis >= tiempo) {
-
-    previousMillis = currentMillis;
-    if (fase < 6) {
-      fase = fase + 1;
-    }
-    else {
-      fase = 1;
-
-    }
-    
-    Serial.println(fase);
-    
-    //Phase1 C-B
-    switch (fase) {
-      case 1:
-        analogWrite(AA1, 0);
-        analogWrite(AA2, 0);
-        analogWrite(BB1, 0);
-        analogWrite(CC2, 0);
-        analogWrite(BB2, mx);
-        analogWrite(CC1, mx);
-        break;
-
-      //Phase2 A-B
-      case 2:
-        analogWrite(AA2, 0);
-        analogWrite(BB1, 0);
-        analogWrite(CC1, 0);
-        analogWrite(CC2, 0);
-        analogWrite(AA1, mx);
-        analogWrite(BB2, mx);
-        break;
-
-      //Phase3 A-C
-      case 3:
-        analogWrite(AA2, 0);
-        analogWrite(BB1, 0);
-        analogWrite(BB2, 0);
-        analogWrite(CC1, 0);
-        analogWrite(CC2, mx);
-        analogWrite(AA1, mx);
-        break;
-
-      //Phase4 B-C
-      case 4:
-        analogWrite(AA1, 0);
-        analogWrite(AA2, 0);
-        analogWrite(BB2, 0);
-        analogWrite(CC1, 0);
-        analogWrite(BB1, mx);
-        analogWrite(CC2, mx);
-        break;
-
-      //Phase5 B-A
-      case 5:
-        analogWrite(AA1, 0);
-        analogWrite(BB2, 0);
-        analogWrite(CC1, 0);
-        analogWrite(CC2, 0);
-        analogWrite(AA2, mx);
-        analogWrite(BB1, mx);
-        break;
-
-      //Phase6 C-A
-      case 6:
-        analogWrite(AA1, 0);
-        analogWrite(BB1, 0);
-        analogWrite(BB2, 0);
-        analogWrite(CC2, 0);
-        analogWrite(CC1, mx);
-        analogWrite(AA2, mx);
-        break;
 
 
 
-    }
+    //  ///////////  -- 1 --  ///////////////////////////////////////////////////
+    phase(AL, BH, CH, AH, BL, CL);
+    //  ///////////  -- 2 --  ///////////////////////////////////////////////////
+    phase(AL, BH, CL, BL, CH, AH);
+    //  //  ///////////  -- 3 --  ///////////////////////////////////////////////////
+    phase(AH, BH, CL, CH, AL, BL);
+    //  //  ///////////  -- 4 --  ///////////////////////////////////////////////////
+    phase(AH, BL, CL, AL, BH, CH);
+    //  //  ///////////  -- 5 --  ///////////////////////////////////////////////////
+    phase(AH, BL, CH, BH, CL, AL);
+    //  //  ///////////  -- 6 --  ///////////////////////////////////////////////////
+    phase(AL, BL, CH, CL, AH, BH);
 
-
-
-
-  }
 }
